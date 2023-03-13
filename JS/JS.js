e6b9b7c969c8181917948cd6a2db3663ba167a0e
raedownen <rae.l.downen@gmail.com>
2023-03-13 16:44:10 -0500

    first commit

---
+++ JS/JS.js
@@ -0,0 +1,18 @@
+"use strict";
+
+console.log('This is my js file');
+var mySchool = "codeup";
+
+console.log("MySchool is awesome!!");
+console.log(mySchool, "is awsome!");
+
+alert("Hey this is an Alert");
+var likePizza = confirm("Do you like Pizza?")
+console.log(likePizza);
+
+var bestToppings = prompt("What toppings do you like on your pizza?");
+//return a string
+console.log(bestToppings);
+
+
+
