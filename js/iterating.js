272b7965755dcdfc030438a10f5e09034d42823f
Consuella Moore <consuellamoore123@gmail.com>
2023-03-20 09:41:06 -0500

    update

--- js/iterating.js
+++ js/iterating.js
@@ -19,22 +19,26 @@
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
- console.log(names)
+    // console.log(names)[0];
+    // console.log(names)[1];
+    // console.log(names)[2];
+    // console.log(names)[3];
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-  for(var i = 0; i < names.length; i++){
-      console.log(names)
+    for(var i = 0; i < names.length; i++){
+        console.log(names[0]);
+
   }
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-    names.forEach(function (names){
-        console.log(names)
-    });
+    // names.forEach(function (name){
+    //     console.log(name)
+    // });
 
     /**
      * TODO:
@@ -49,9 +53,23 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
-    var names = ['Connie',  'Jakira', 'Melissa', 'Mark'];
-    console.log(names[0]);
-    console.log(names[1]);
-    console.log(names[3]);
+
+    function first(arr){
+        return arr[0];
+    }
+    console.log(first(numbers);
+
+    function second(arr){
+        return arr[1];
+    }
+    console.log(second(numbers));
+
+    function last() {
+        return [arr.length];
+    }
+    console.log(last(numbers));
+
+
+
 
 })();
