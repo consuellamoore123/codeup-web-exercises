b6ae8533bf05ba9144a21eb8ba5feebcb9f2514d
Consuella Moore <consuellamoore123@gmail.com>
2023-03-24 22:53:29 -0500

    update

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -6,17 +6,19 @@ var input = prompt("Please provide an odd number");
 
 
 
+
+
 for(var i = 1; i < 100; i++) {
     if (i % 2 === 0) {
         continue
     }
-    if (i === input){
-        console.log("skipping" + input);
+    if (i == input){
+        continue
     }
     else {
         console.log("Here is an Odd number:" + i)
     }
     if (i>49){
-        break;
+        break
     }
 }
