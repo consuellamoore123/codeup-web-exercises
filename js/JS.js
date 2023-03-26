67daee055d0f37d740c8db88912816309b661b03
Consuella Moore <consuellamoore123@gmail.com>
2023-03-26 13:10:41 -0500

    update

--- js/JS.js
+++ js/JS.js
@@ -270,3 +270,145 @@
 //     }
 // }
 // console.log(upperCase("be"))
+
+
+
+//Define a function named isANumber that takes in a value
+// and returns true if the input is numeric or not.
+// // Numeric strings are not considered as numbers and should return false.
+// function isANumber (input) {
+//     if (typeof input === "number") {
+//         return true;
+//     } else{
+//         return false
+//     }
+//
+// }
+// isANumber
+// Define a function named increment that takes in an input
+// and adds 1 to it if the input is numeric.
+// If the input is not numeric, then return false.
+
+// var increment  = function (input) {
+//     if(typeof input === "number") {
+//         return input + 1
+//     } else
+//     {
+//         return false
+//     }
+// }
+//Define a function named decrement. If the provided input is numeric,
+//the function should subtract 1 and return the result.
+//If the input is not numeric, decrement should return false.
+
+// var decrement  = function (input) {
+//     if (typeof input === "number") {
+//         return input - 1
+//     } else{
+//         return false
+//     }
+// }
+
+// Define a function named parseNumber that parses (converts) a numeric String
+// and returns it's ' + 'value as a number, the function should be able to decide
+// if it should be parsing an Integer or a Float value.
+//
+// function parseNumber (input) {
+//     return (parseInt(input) || parseFloat(input))
+//
+//
+// }
+
+
+
+
+// Write a function named add that takes in two inputs. If both inputs provided are numeric,
+// add will return the sum of both inputs. If one or both inputs is not numeric,
+// add should return false.
+
+// function add (x, y) {
+//     if (isNaN(x) || isNaN(y) || typeof x === "boolean" || typeof y === "boolean") {
+//         return false;
+//     } else if (x,y) {
+//         var num1 = Number(x)
+//         var num2 = Number(y)
+//         return num1 + num2
+//     }
+// }
+
+
+//Write a function named multiply that takes in two inputs.
+// If both inputs provided are numeric,
+// multiply returns the product of the first input multiplied by the second.
+// If either or both inputs are not numeric, multiply should return false.
+//
+// function multiply (x, y) {
+//     if (isNaN(x) || isNaN(y) || typeof x === "boolean" || typeof y === "boolean") {
+//         return false;
+//     } else if (x, y) {
+//         var num1 = Number(x)
+//         var num2 = Number(y)
+//         return num1 * num2
+//     }
+// }
+
+//Define a function named square. If the provided input is numeric,
+// square should return the number multiplied by itself.
+// If the input is not numeric, square, should return false.
+//
+// function square (input){
+//     if(typeof input != "number"){
+//         return false
+//     } else {
+//         return Math.pow(input,2)
+//     }
+// }
+
+//Write a function named sumOfSquares that takes in two inputs.
+// If both inputs are numeric, sumOfSquares should square each input
+// and return the sum of them added together. For example,
+// if we pass a and b then we should square a and add it to the square of b.
+// If either or both inputs are not numeric, sumOfSquares should return false.
+//
+// function sumOfSquares(x, y) {
+//     if (isNaN(x) || isNaN(y)){
+//         return false
+//     } else if (typeof x == "boolean" || typeof y == "boolean"){
+//         return false
+//     }
+//     else{
+//         return Math.pow(x, 2)  + Math.pow(y, 2);
+//     }
+// }
+
+//Write a function named isPalindrome that takes in a single input.
+// If the input is a numeric string, then return false. If the input is a string,
+// then return true if the string is the same forwards as it is backwards.
+// Return false if the string is different forwards from backwards.
+// Ignore capitalization.
+
+// function isPalindrome(input){
+//     if (input == true) {
+//         return input.split().reverse().join()
+//     } else if (input.split().reverse().join() === input) {
+//         return true
+//     }else {
+//         return false
+//     }
+// }
+
+
+// Write a function named getHighestNumber that takes in 3 arguments.
+// If all 3 inputs are numbers or numeric strings, then return the highest number.
+// If any of the 3 inputs is missing or non-numeric, then return false.
+
+// function getHighestNumber (x, y, z) {
+//     if (typeof (x, y, z) != "number") {
+//         return false
+//     }else {
+//         return Math.max(x, y, z)
+//     }
+// }
+
+
+
