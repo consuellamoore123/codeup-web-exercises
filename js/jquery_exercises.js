634356e1608bb7af7694244bd34ae91a8dc54505
Consuella Moore <consuellamoore123@gmail.com>
2023-04-19 12:42:58 -0500

    update

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -10,5 +10,9 @@
 
 $('.list').css('font-size', '20px');
 
+// $('h1').css('background-color', 'yellow');
+// $('p').css('background-color', 'yellow');
+// $('li').css('background-color', 'yellow');
+
 
 $('h1, p, li').css('background-color', 'yellow');
