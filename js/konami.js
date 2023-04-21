1d648cef09a8af377844133dce3dad52f124826a
Consuella Moore <consuellamoore123@gmail.com>
2023-04-20 22:36:26 -0500

    continued creating wheel

--- js/konami.js
+++ js/konami.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
 var str = ""
 $(document).on("keyup", function(e){
     str = str + e.keyCode
@@ -10,6 +11,8 @@ $(document).on("keyup", function(e){
     }
 });
 
+
+"use Strict"
 
 
 $(document).ready(function() {
@@ -44,17 +47,60 @@ $(document).ready(function() {
                     $("#result").text("You Lose! The word was " + word + ".");
                 }
             }
-            $(this).attr("disabled", true);
+            $(this).attributes("disabled", true);
         });
         $("#buttons").append(button);
     }
 });
 
-
+"use strict"
 $(document).ready(function() {
-    var angle = 45;
-    $(this).each(function() {
-        $(".pin").css("transform", "rotate(" + angle + "deg)");
-        angle += 45;
-    });
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+
+
+
+//Create the wheel and add colors
+
+var colors = ["red", "blue", "yellow", "orange", "green", "purple", "pink", "lime"];
+var segments = ["Prize 1", "Prize 2", "Prize 3", "Prize 4", "Prize 5", "Prize 6", "Prize 7", "Prize 8"];
+var startAngle = 0;
+var arc = Math.PI / 4;
+var centerX = canvas.width / 2;
+var centerY = canvas.height / 2;
+var radius = 200;
+
+for (var i = 0; i < segments.length; i++) {
+    var angle = startAngle + i * arc;
+    context.beginPath();
+    context.fillStyle = colors[i % colors.length];
+    context.moveTo(centerX, centerY);
+    context.arc(centerX, centerY, radius, angle, angle + arc, false);
+    context.lineTo(centerX, centerY);
+    context.fill();
+    context.save();
+
+    // Add the segment text here
+}
+for (var i = 0; i < segments.length; i++) {
+    var angle = startAngle + i * arc;
+    context.beginPath();
+    context.fillStyle = colors[i % colors.length];
+    context.moveTo(centerX, centerY);
+    context.arc(centerX, centerY, radius, angle, angle + arc, false);
+    context.lineTo(centerX, centerY);
+    context.fill();
+    context.save();
+
+    // Add the segment text here
+    context.fillStyle = "#FFFFFF";
+    context.translate(centerX, centerY);
+    context.rotate(angle + arc / 2);
+    context.textAlign = "center";
+    context.fillText(segments[i], radius / 2, 0);
+    context.restore();
+}
+
 });
+
+//Add a click event listener to the button that will spin the wheel using a random number generator. You can use the Math.random() method to generate a random number between 0 and 1, and then use the Math.floor() method to round down to the nearest integer. Multiply the random number
