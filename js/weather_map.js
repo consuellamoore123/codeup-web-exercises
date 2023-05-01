b38491a94aec39e58fc0febe345fe4dc685e3fe9
Consuella Moore <consuellamoore123@gmail.com>
2023-04-30 23:49:34 -0500

    added variables

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,6 +1,15 @@
 
 
-// var submitbtn =document.querySelector('.btn')
-// var inputValue = document.querySelector('.form-control')
-// var name = document.querySelector('.name')
+const timeEl = document.getElementById("time");
+const dateEl = document.getElementById("date");
+const currentWeatherItemEl = docuemnt.getElementByID("current-weather-items");
+const timezone = document.getElementById("time-zone");
+const countryEl = document.getElementById("country");
+const weatherForecastEl = document.getElementById(weather-forecast);
+const currentTempEl = document.getElementById("current-Temp");
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+"Jul", "Aug", "Sep","Oct", "Nov", "Dec"]
+
 
