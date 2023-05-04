4aea8fc6621f7d0c559c4ffd3b3e1f4b56d0cf70
Consuella Moore <consuellamoore123@gmail.com>
2023-05-03 20:01:02 -0500

    adding styling

--- js/weather_map.js
+++ js/weather_map.js
@@ -2,28 +2,6 @@
     "use strict"
 
 
-mapboxgl.accessToken = MAPBOX_APPID
-
-var map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    zoom: 15, // starting zoom
-    center: [-98.65177, 29.41824] // [lng, lat]
-
-});
-
-function geocode(search, token) {
-    var baseUrl = 'https://api.mapbox.com';
-    var endPoint = '/geocoding/v5/mapbox.places/';
-    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
-        .then(function (res) {
-            return res.json();
-            // to get all the data from the request, comment out the following three lines...
-        }).then(function (data) {
-            return data.features[0].center;
-        });
-}
-
 // weathermap
 
 const timeEl = document.getElementById("time");
@@ -38,7 +16,7 @@ const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", 
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
     "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
-
+//
 // // Set Time and Date
 // setInterval(() => {
 //     const time = new Date();
@@ -49,102 +27,149 @@ const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
 //     const hoursIn12HrFormat = hour >= 13 ? hour % 12 : hour
 //     const minutes = time.getMinutes();
 //     const ampm = hour >= 12 ? "PM" : "AM"
-
-    // timeEl.innerHTML = (hoursIn12HrFormat < 10 ? "0" + hoursIn12HrFormat : hoursIn12HrFormat) + ":" + (minutes < 10 ? "0" + minutes : minutes) + `<span id="am-pm">${ampm}</span>`
-
-//     dateEl.innerHTML = days[day] + ", " + date+ " " + months[month]
-// }, 1000);
-
-getWeatherData()
-
-//Get GeoLocation
-function getWeatherData() {
-    navigator.geolocation.getCurrentPosition((success) => {
-        console.log(success);
-
-        let {latitude, longitude} = success.coords;
-        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly, minutely&units=metric&appid=${OPEN_WEATHER_APPID}`).then(res => res.json()).then(data => {
-            console.log(data);
-            showWeatherData(data);
-
-        });
-    });
-}
 //
-function showWeatherData(data) {
-    let {humidity, pressure, sunrise, sunset, wind_speed} = data.current;
+//     timeEl.innerHTML = (hoursIn12HrFormat < 10 ? "0" + hoursIn12HrFormat : hoursIn12HrFormat) + ":" + (minutes < 10 ? "0" + minutes : minutes) + `<span id="am-pm">${ampm}</span>`
+//
+// <!--       dateEl.innerHTML = days[day] + ", " + date+ " " + months[month]-->
+//     <!--}, 1000);-->
+//
 
-    timezone.innerHTML = data.timezone;
-    countryEl.innerHTML = data.lat + "N " + data.lon + "w ";
 
-    currentWeatherItemEl.innerHTML =
-        `<div class="weather-item">
-    <div>Humidity</div>
-    <div>${humidity}</div>
-</div>
-<div class="weather-item">
-    <div>pressure</div>
-    <div>${pressure}</div>
-</div>
-<div class="weather-item">
-    <div>Wind Speed</div>
-    <div>${wind_speed}</div>
-</div>
-<div class="weather-item">
-    <div>Sunrise</div>
-    <div>${window.moment(sunrise * 1000).format('HH:mm a')}</div>
-</div>
-<div class="weather-item">
-    <div>Sunset</div>
-    <div>${window.moment(sunset * 1000).format('HH:mm a')}</div>
+// Get Weather Data
+    const getWeatherData = (lat, lng, key) => {
+        navigator.geolocation.getCurrentPosition((success) => {
+            console.log(success);
+
+            // let {latitude, longitude} = success.coords;
+            let latitude = lat
+            let longitude = lng
+
+            fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly, minutely&units=imperial&appid=${key}`).then(res => res.json()).then(data => {
+                console.log(data);
+                // showWeatherData(data);
+                cardMarkup(data);
+
+            });
+        });
+    }
+
+    getWeatherData(29.41824, -98.65177, OPEN_WEATHER_APPID);
+// Creating Cards
+    const cardMarkup = (data) => {
+        console.log(data.daily);
+        let markup = "";
+        data.daily.forEach((day, i) => {
+            if (i < 5) {
+                const humidity = day.humidity;
+                const pressure = day.pressure;
+                const windSpeed = day.wind_speed;
+                const tempDay = day.temp.day;
+                const tempNight = day.temp.night
+                const sunrise = day.sunrise
+                const sunset = day.sunset
+                const iconSource = `https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`
+
+                markup += `
+<div class="card text-center">
+  <div class="card-header">
+   ${window.moment(day.dt * 1000).format("dddd")}
+
+   
+  </div>
+  <div class="card-body">
+      <img src="${iconSource}">
+      
+    <h6 class="card-title">Day: ${tempDay}&#8457;</h6>
+    <h6 class="card-title">Night: ${tempNight}&#8457;</h6>
+    <br>
+    <h6 class="card-title">Humidity: ${humidity}</h6>
+    <h6 class="card-title">Pressure: ${pressure}</h6>
+    <h6 class="card-title">Wind Speed: ${windSpeed}</h6>
+    <br>
+    
+    <h6 class="card-title">Sunrise: ${window.moment(sunrise * 1000).format('HH:mm a')}</h6>
+    <h6 class="card-title">Sunset: ${window.moment(sunset * 1000).format('HH:mm a')}</h6>
+   
+  </div>
+  <div class="card-footer text-muted"></div>
 </div>
 
 `;
+            }
+
+        })
+        $(".card-container").html(markup)
 
 
-    let otherDayForecast = ""
-    data.daily.forEach((day, idx) => {
-        if(idx == 0){
-            currentTempEl.innerHTML = `
-        <img src="https://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
-        <div class="other">
-        <div class="day">${window.moment(day.dt * 1000).format("ddd")}</div>
-        <div class="temp">Night - ${day.temp.night}&#176; C</div>
-        <div class="temp">Day - ${day.temp.day}&#176; C</div>
-        </div>`
 
-        } else {
-            otherDayForecast += `
-        <div class="weather-forecast-item">
-         <div class="day">${window.moment(day.dt * 1000).format("ddd")}</div>
-            <img src=" https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather Icon" class="w-icon">
-            <div class="temp">Night - ${day.temp.night}&#176; C</div>
-            <div class="temp">Day - ${day.temp.day}&#176; C</div>
-        </div>`
+
+    }
+// Creating map
+
+    mapboxgl.accessToken = MAPBOX_APPID
+
+    var map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v12', // style URL
+        zoom: 15, // starting zoom
+        center: [-98.65177, 29.41824] // [lng, lat]
+
+    });
+
+
+    let marker;
+
+    map.on("click", (e) => {
+        var lng = (e.lngLat.lng);
+        var lat = (e.lngLat.lat);
+        reverseGeocode({lng: lng, lat: lat}, MAPBOX_APPID)
+        getWeatherData(lat, lng, OPEN_WEATHER_APPID)
+        if (marker) {
+            marker.remove()
         }
-       // let search = function(){
-        //   this.fetchWeather(document.querySelector(".btn").value)
+        marker = new mapboxgl.Marker()
+            .setLngLat([lng, lat])
+            .addTo(map)
+        map.setCenter([lng, lat]);
+        map.setZoom(15)
+
     })
-    weatherForecastEl.innerHTML = otherDayForecast;
-}
+// add zoom to map
+    map.addControl(new mapboxgl.NavigationControl());
 
 
-// showWeatherData().addEventListener('click', searchWeather);
-//     currentWeatherItemEl
-// });
+    const geocodeAddressMarker = (address, token) => {
+        geocode(address, token).then((res) => {
+            console.log(res)
+            const lng = res[0]
+            const lat = res[1]
+            if (marker) {
+                marker.remove()
+            }
+            marker = new mapboxgl.Marker()
+                .setLngLat([lng, lat])
+                .addTo(map)
+            map.setCenter([lng, lat]);
+            map.setZoom(15)
+            getWeatherData(lat, lng, OPEN_WEATHER_APPID);
+        })
+    }
 
-map.on("click", (e) => {
-    var lng = (e.lngLat.lng);
-    var lat = (e.lngLat.lat);
-    reverseGeocode({lng: lng, lat: lat}, MAPBOX_APPID)
-})
 
+    const searchButton = $(`.btn`)
+    searchButton.on("click", function (e) {
+        e.preventDefault()
+        const userInput = $(`.form-control`).val()
+        console.log('click')
+        console.log(userInput)
+        geocodeAddressMarker(userInput, MAPBOX_APPID)
 
-
-
+    });
 
 
 })();
+
+
 
 
 
